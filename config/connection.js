e296ae81cb5ea18d902521e7bb3d65d8554f735a
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-07-02 17:50:25 -0700

    added models

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 
 let sequelize;
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-  //only these statements if not using heroku
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
